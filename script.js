9147df825b9b82f8d627738c0486bbd650fe7a51
abinob01 <abinob70@gmail.com>
2025-10-24 06:18:00 -0700

    Manuplating the OK button with JavaScript

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 "use strict";
+const button = Document.querySelector("button");
+butoon.addEventListener("click", function () {
+  const input = Document.querySelector("input");
+  const name = input.value;
+  alert("Hello " + name + "! Welcome to My First Course Trial");
+});
